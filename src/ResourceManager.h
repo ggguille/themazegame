ecd23949e1386aee9e6c60910c02becb77807df3
Guille <g.granado.g@gmail.com>
2020-03-18 21:35:04 +0100

    Resource manager (#2)
    * create ResourceManager class
    
    * use resource manager
    
    Co-authored-by: ggguille <999uille@gmail.com>

---
+++ src/ResourceManager.h
@@ -0,0 +1,19 @@
+#ifndef RESOURCE_MANAGER_H
+#define RESOURCE_MANAGER_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class ResourceManager
+{
+public:
+  ResourceManager();
+  vector<string> fetch(string path);
+private:
+  string resource_path;
+};
+
+
+#endif // RESOURCE_MANAGER_H
